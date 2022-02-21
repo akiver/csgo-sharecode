56eb68f2c46a3aa3b45c8021af220fd7711e4e19
AkiVer <ys.renaud@gmail.com>
2022-02-21 18:04:11 +0100

    feat: decode/encode crosshair share code
    - Export CJS/ESM module
    - Throw explicit errors
    - Rename decode/encode to decodeMatchShareCode/encodeMatchShareCode
    - Rename ShareCode interface to MatchInformation
    - Upgrade deps
    - Replace jest by vitest
    - Remove renovate

--- README.md
+++ README.md
@@ -1,26 +1,29 @@
 # csgo-sharecode
 
-Node module to decode / encode the CSGO share codes used to share game replays between players.
+JS module to decode / encode CSGO share codes used to share game replays/crosshairs between players.
 
 # Installation
 
-`npm i csgo-sharecode` or `yarn add csgo-sharecode`
+`npm install csgo-sharecode`
 
-**Since the version 2, this module relies on native [BigInt](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/BigInt) which is available since Node 10.8.0. For Node < 10.8.0, please use the version 1.**
+**This module relies on [BigInt](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/BigInt), your target platform must support it!**
 
 # Usage
 
-## Decode
+## Match
 
-Decode a share code from its string to an object.
+### Decoding
 
-```js
-const { decode } = require('csgo-sharecode');
+Decodes a match share code into a `MatchInformation` object.
+
+```ts
+import { decodeMatchShareCode, MatchInformation } from 'csgo-sharecode';
 
 const shareCode = 'CSGO-GADqf-jjyJ8-cSP2r-smZRo-TO2xK';
-const info = decode(shareCode);
-console.log(info);
+const matchInformation: MatchInformation = decodeMatchShareCode(shareCode);
+console.log(matchInformation);
 // output:
+//
 // {
 //    matchId: 3230642215713767580n,
 //    reservationId: 3230647599455273103n,
@@ -28,24 +31,157 @@ console.log(info);
 // }
 ```
 
-## Encode
+### Encoding
 
-Encode a [CDataGCCStrike15_v2_MatchInfo](https://github.com/SteamDatabase/Protobufs/blob/master/csgo/cstrike15_gcmessages.proto) (lookup for `CDataGCCStrike15_v2_MatchInfo`) message into a share code (string).  
-The object `match` used in the example below use values coming from a real _CDataGCCStrike15_v2_MatchInfo_ message.  
-You should get it from the _Steam Game Coordinator_ or from a _.info_ file.
+Encodes a `MatchInformation` object into a match share code.
+The example below use values coming from a real [CDataGCCStrike15_v2_MatchInfo](https://github.com/SteamDatabase/Protobufs/blob/master/csgo/cstrike15_gcmessages.proto) (lookup for `CDataGCCStrike15_v2_MatchInfo`) message.  
+You should get them from the _Steam Game Coordinator_ or from a _.info_ file.
 
-```js
-const { encode } = require('csgo-sharecode');
+```ts
+import { encodeMatch, MatchInformation } from 'csgo-sharecode';
 
-const match = {
+const matchInformation: MatchInformation = {
   matchId: BigInt('3230642215713767580'),
   reservationId: BigInt('3230647599455273103'),
   tvPort: 599906796,
 };
 
-const shareCode = encode(match.matchId, match.reservationId, match.tvPort);
+const shareCode = encodeMatch(matchInformation);
 console.log(shareCode);
-// output: "CSGO-GADqf-jjyJ8-cSP2r-smZRo-TO2xK"
+// output:
+//
+// "CSGO-GADqf-jjyJ8-cSP2r-smZRo-TO2xK"
+```
+
+## Crosshair
+
+### Decoding
+
+Decodes a crosshair share code into a `Crosshair` object.
+
+```ts
+import { decodeCrosshairShareCode, Crosshair } from 'csgo-sharecode';
+
+const shareCode = 'CSGO-Cn37R-YE7vo-pLCAL-aURmZ-z6zkG';
+const crosshair: Crosshair = decodeCrosshairShareCode(shareCode);
+console.log(crosshair);
+// output:
+//
+// {
+//   gap: -1.3,
+//   outline: 2,
+//   red: 175,
+//   green: 81,
+//   blue: 213,
+//   alpha: 137,
+//   splitDistance: 6,
+//   fixedCrosshairGap: 3,
+//   color: 5,
+//   outlineEnabled: true,
+//   innerSplitAlpha: 0.6,
+//   outerSplitAlpha: 0.4,
+//   splitSizeRatio: 0.5,
+//   thickness: 1.2,
+//   centerDotEnabled: true,
+//   deployedWeaponGapEnabled: true,
+//   alphaEnabled: true,
+//   tStyleEnabled: true,
+//   style: 2,
+//   length: 4.6
+// }
+```
+
+### Encoding
+
+Encodes a `Crosshair` object into a crosshair share code.
+
+```ts
+import { encodeCrosshair, Crosshair } from 'csgo-sharecode';
+
+const crosshair: Crosshair = {
+  gap: -1.3,
+  outline: 2,
+  red: 175,
+  green: 81,
+  blue: 213,
+  alpha: 137,
+  splitDistance: 6,
+  fixedCrosshairGap: 3,
+  color: 5,
+  innerSplitAlpha: 0.6,
+  outlineEnabled: true,
+  outerSplitAlpha: 0.4,
+  splitSizeRatio: 0.5,
+  thickness: 1.2,
+  centerDotEnabled: true,
+  alphaEnabled: true,
+  tStyleEnabled: true,
+  style: 2,
+  length: 4.6,
+  deployedWeaponGapEnabled: true,
+};
+
+const shareCode = encodeCrosshair(crosshair);
+console.log(shareCode);
+// output:
+//
+// "CSGO-Cn37R-YE7vo-pLCAL-aURmZ-z6zkG"
+```
+
+### Generating CSGO ConVars
+
+Utility function to generate CSGO ConVars for a given crosshair.
+
+```ts
+import { crosshairToConVars, Crosshair } from 'csgo-sharecode';
+
+const crosshair: Crosshair = {
+  gap: -1.3,
+  outline: 2,
+  red: 175,
+  green: 81,
+  blue: 213,
+  alpha: 137,
+  splitDistance: 6,
+  fixedCrosshairGap: 3,
+  color: 5,
+  innerSplitAlpha: 0.6,
+  outlineEnabled: true,
+  outerSplitAlpha: 0.4,
+  splitSizeRatio: 0.5,
+  thickness: 1.2,
+  centerDotEnabled: true,
+  alphaEnabled: true,
+  tStyleEnabled: true,
+  style: 2,
+  length: 4.6,
+  deployedWeaponGapEnabled: true,
+};
+
+const conVars = crosshairToConVars(crosshair);
+console.log(conVars);
+// Output:
+//
+// cl_crosshair_drawoutline "1"
+// cl_crosshair_dynamic_maxdist_splitratio "0.5"
+// cl_crosshair_dynamic_splitalpha_innermod "0.6"
+// cl_crosshair_dynamic_splitalpha_outermod "0.4"
+// cl_crosshair_dynamic_splitdist "6"
+// cl_crosshair_outlinethickness "2"
+// cl_crosshair_t "1"
+// cl_crosshairalpha "137"
+// cl_crosshaircolor "5"
+// cl_crosshaircolor_b "213"
+// cl_crosshaircolor_g "81"
+// cl_crosshaircolor_r "175"
+// cl_crosshairdot "1"
+// cl_crosshairgap "-1.3"
+// cl_crosshairgap_useweaponvalue "1"
+// cl_crosshairsize "4.6"
+// cl_crosshairstyle "2"
+// cl_crosshairthickness "1.2"
+// cl_crosshairusealpha "1"
+// cl_fixedcrosshairgap "3"
 ```
 
 # License
---
+++ src/index.ts
@@ -0,0 +1,238 @@
+const DICTIONARY = 'ABCDEFGHJKLMNOPQRSTUVWXYZabcdefhijkmnopqrstuvwxyz23456789';
+const DICTIONARY_LENGTH = BigInt(DICTIONARY.length);
+const SHARECODE_PATTERN = /^CSGO(-?[\w]{5}){5}$/;
+
+export interface MatchInformation {
+  matchId: bigint;
+  reservationId: bigint;
+  tvPort: number;
+}
+
+export interface Crosshair {
+  length: number;
+  red: number;
+  green: number;
+  blue: number;
+  gap: number;
+  alphaEnabled: boolean;
+  alpha: number;
+  outlineEnabled: boolean;
+  outline: number;
+  color: number;
+  thickness: number;
+  centerDotEnabled: boolean;
+  splitDistance: number;
+  fixedCrosshairGap: number;
+  innerSplitAlpha: number;
+  outerSplitAlpha: number;
+  splitSizeRatio: number;
+  tStyleEnabled: boolean;
+  deployedWeaponGapEnabled: boolean;
+  /**
+   * 0 => Default
+   * 1 => Default static
+   * 2 => Classic
+   * 3 => Classic dynamic
+   * 4 => Classic static
+   */
+  style: number;
+}
+
+export class InvalidShareCode extends Error {
+  public constructor() {
+    super('Invalid share code');
+  }
+}
+
+export class InvalidCrosshairShareCode extends Error {
+  public constructor() {
+    super('Invalid crosshair share code');
+  }
+}
+
+function bytesToHex(bytes: number[]): string {
+  return Array.from(bytes, (byte) => {
+    return ('0' + (byte & 0xff).toString(16)).slice(-2);
+  }).join('');
+}
+
+function bytesToBigInt(bytes: number[]): bigint {
+  const hex = bytesToHex(bytes);
+
+  return BigInt(`0x${hex}`);
+}
+
+function stringToBytes(str: string): number[] {
+  const bytes: number[] = [];
+
+  for (let i = 0; i < str.length; i += 2) {
+    bytes.push(parseInt(str.slice(i, i + 2), 16));
+  }
+
+  return bytes;
+}
+
+function int16ToBytes(number: number): number[] {
+  return [(number & 0x0000ff00) >> 8, number & 0x000000ff];
+}
+
+function uint8ToInt8(number: number) {
+  return (number << 24) >> 24;
+}
+
+function sumArray(array: number[]) {
+  return array.reduce((previousValue, value) => {
+    return previousValue + value;
+  }, 0);
+}
+
+function shareCodeToBytes(shareCode: string) {
+  if (!shareCode.match(SHARECODE_PATTERN)) {
+    throw new InvalidShareCode();
+  }
+
+  shareCode = shareCode.replace(/CSGO|-/g, '');
+  const chars = Array.from(shareCode).reverse();
+  let big = BigInt(0);
+  for (let i = 0; i < chars.length; i++) {
+    big = big * DICTIONARY_LENGTH + BigInt(DICTIONARY.indexOf(chars[i]));
+  }
+
+  const str = big.toString(16).padStart(36, '0');
+  const bytes = stringToBytes(str);
+
+  return bytes;
+}
+
+function bytesToShareCode(bytes: number[]) {
+  const hex = bytesToHex(bytes);
+  let total = BigInt(`0x${hex}`);
+  let chars = '';
+  let rem = BigInt(0);
+  for (let i = 0; i < 25; i++) {
+    rem = total % DICTIONARY_LENGTH;
+    chars += DICTIONARY[Number(rem)];
+    total = total / DICTIONARY_LENGTH;
+  }
+
+  return `CSGO-${chars.slice(0, 5)}-${chars.slice(5, 10)}-${chars.slice(10, 15)}-${chars.slice(15, 20)}-${chars.slice(
+    20,
+    25
+  )}`;
+}
+
+/**
+ * Match fields should come from a CDataGCCStrike15_v2_MatchInfo protobuf message.
+ * https://github.com/SteamDatabase/Protobufs/blob/master/csgo/cstrike15_gcmessages.proto (lookup for `CDataGCCStrike15_v2_MatchInfo`).
+ */
+export function encodeMatch({ matchId, reservationId, tvPort }: MatchInformation): string {
+  const matchBytes = stringToBytes(matchId.toString(16)).reverse();
+  const reservationBytes = stringToBytes(reservationId.toString(16)).reverse();
+  const tvBytes = int16ToBytes(tvPort).reverse();
+  const bytes = [...matchBytes, ...reservationBytes, ...tvBytes];
+  const shareCode = bytesToShareCode(bytes);
+
+  return shareCode;
+}
+
+export function decodeMatchShareCode(shareCode: string): MatchInformation {
+  const bytes = shareCodeToBytes(shareCode);
+
+  return {
+    matchId: bytesToBigInt(bytes.slice(0, 8).reverse()),
+    reservationId: bytesToBigInt(bytes.slice(8, 16).reverse()),
+    tvPort: Number(bytesToBigInt(bytes.slice(16, 18).reverse())),
+  };
+}
+
+export function decodeCrosshairShareCode(shareCode: string): Crosshair {
+  const bytes = shareCodeToBytes(shareCode);
+  const size = sumArray(bytes.slice(1)) % 256;
+
+  if (bytes[0] !== size) {
+    throw new InvalidCrosshairShareCode();
+  }
+
+  const crosshair: Crosshair = {
+    gap: uint8ToInt8(bytes[2]) / 10,
+    outline: bytes[3] / 2,
+    red: bytes[4],
+    green: bytes[5],
+    blue: bytes[6],
+    alpha: bytes[7],
+    splitDistance: bytes[8],
+    fixedCrosshairGap: uint8ToInt8(bytes[9]) / 10,
+    color: bytes[10] & 7,
+    outlineEnabled: (bytes[10] & 8) === 8,
+    innerSplitAlpha: (bytes[10] >> 4) / 10,
+    outerSplitAlpha: (bytes[11] & 0xf) / 10,
+    splitSizeRatio: (bytes[11] >> 4) / 10,
+    thickness: bytes[12] / 10,
+    centerDotEnabled: ((bytes[13] >> 4) & 1) === 1,
+    deployedWeaponGapEnabled: ((bytes[13] >> 4) & 2) === 2,
+    alphaEnabled: ((bytes[13] >> 4) & 4) === 4,
+    tStyleEnabled: ((bytes[13] >> 4) & 8) === 8,
+    style: (bytes[13] & 0xf) >> 1,
+    length: bytes[14] / 10,
+  };
+
+  return crosshair;
+}
+
+export function encodeCrosshair(crosshair: Crosshair): string {
+  const bytes: number[] = [
+    0,
+    1,
+    (crosshair.gap * 10) & 0xff,
+    crosshair.outline * 2,
+    crosshair.red,
+    crosshair.green,
+    crosshair.blue,
+    crosshair.alpha,
+    crosshair.splitDistance,
+    (crosshair.fixedCrosshairGap * 10) & 0xff,
+    (crosshair.color & 7) | (Number(crosshair.outlineEnabled) << 3) | ((crosshair.innerSplitAlpha * 10) << 4),
+    (crosshair.outerSplitAlpha * 10) | ((crosshair.splitSizeRatio * 10) << 4),
+    crosshair.thickness * 10,
+    (crosshair.style << 1) |
+      (Number(crosshair.centerDotEnabled) << 4) |
+      (Number(crosshair.deployedWeaponGapEnabled) << 5) |
+      (Number(crosshair.alphaEnabled) << 6) |
+      (Number(crosshair.tStyleEnabled) << 7),
+    crosshair.length * 10,
+    0,
+    0,
+    0,
+  ];
+
+  bytes[0] = sumArray(bytes) & 0xff;
+
+  const shareCode = bytesToShareCode(bytes);
+
+  return shareCode;
+}
+
+export function crosshairToConVars(crosshair: Crosshair): string {
+  return `
+cl_crosshair_drawoutline "${Number(crosshair.outlineEnabled)}"
+cl_crosshair_dynamic_maxdist_splitratio "${crosshair.splitSizeRatio}"
+cl_crosshair_dynamic_splitalpha_innermod "${crosshair.innerSplitAlpha}"
+cl_crosshair_dynamic_splitalpha_outermod "${crosshair.outerSplitAlpha}"
+cl_crosshair_dynamic_splitdist "${crosshair.splitDistance}"
+cl_crosshair_outlinethickness "${crosshair.outline}"
+cl_crosshair_t "${Number(crosshair.tStyleEnabled)}"
+cl_crosshairalpha "${crosshair.alpha}"
+cl_crosshaircolor "${crosshair.color}"
+cl_crosshaircolor_b "${crosshair.blue}"
+cl_crosshaircolor_g "${crosshair.green}"
+cl_crosshaircolor_r "${crosshair.red}"
+cl_crosshairdot "${Number(crosshair.centerDotEnabled)}"
+cl_crosshairgap "${crosshair.gap}"
+cl_crosshairgap_useweaponvalue "${Number(crosshair.deployedWeaponGapEnabled)}"
+cl_crosshairsize "${crosshair.length}"
+cl_crosshairstyle "${crosshair.style}"
+cl_crosshairthickness "${crosshair.thickness}"
+cl_crosshairusealpha "${Number(crosshair.alphaEnabled)}"
+cl_fixedcrosshairgap "${crosshair.fixedCrosshairGap}"
+`;
+}
